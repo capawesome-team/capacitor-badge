1375768e515b7cb3431931c1879d0c2bbd6271cd
Robin Genz <mail@robingenz.dev>
2021-03-20 17:14:32 +0100

    feat: add `ios` support

--- ios/Plugin/BadgePlugin.swift
+++ ios/Plugin/BadgePlugin.swift
@@ -9,10 +9,23 @@ import Capacitor
 public class BadgePlugin: CAPPlugin {
     private let implementation = Badge()
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+    @objc func get(_ call: CAPPluginCall) {
+        let count = implementation.get()
         call.resolve([
-            "value": implementation.echo(value)
+            "count": count
         ])
+    }
+    
+    @objc func set(_ call: CAPPluginCall) {
+        let count = call.getInt("count") ?? 0
+        implementation.set(count: count, completion: {
+            call.resolve()
+        })
+    }
+    
+    @objc func clear(_ call: CAPPluginCall) {
+        implementation.clear(completion: {
+            call.resolve()
+        })
     }
 }

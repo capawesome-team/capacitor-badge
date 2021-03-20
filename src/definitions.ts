62f163ef7b281daab3801151383e80bb4a83b7d2
Robin Genz <mail@robingenz.dev>
2021-03-20 17:13:53 +0100

    feat: add `web` support (#1)
    * feat: add `web` support
    
    * store in locale storage

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,25 @@
 export interface BadgePlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  /**
+   * Get the badge count.
+   * The badge count won't be lost after a reboot or app restart.
+   *
+   * Default: `0`.
+   */
+  get(): Promise<GetBadgeResult>;
+  /**
+   * Set the badge count.
+   */
+  set(options: SetBadgeOptions): Promise<void>;
+  /**
+   * Clear the badge count.
+   */
+  clear(): Promise<void>;
+}
+
+export interface GetBadgeResult {
+  count: number;
+}
+
+export interface SetBadgeOptions {
+  count: number;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,53 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { BadgePlugin } from './definitions';
+import type {
+  BadgePlugin,
+  GetBadgeResult,
+  SetBadgeOptions,
+} from './definitions';
 
 export class BadgeWeb extends WebPlugin implements BadgePlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  private static STORAGE_KEY: string = 'capacitor.badge';
+
+  constructor() {
+    super();
+    this.restore();
+  }
+
+  public async get(): Promise<GetBadgeResult> {
+    const value = localStorage.getItem(BadgeWeb.STORAGE_KEY);
+    const count = value ? parseInt(value, 10) : 0;
+    return { count };
+  }
+
+  public async set(options: SetBadgeOptions): Promise<void> {
+    const count = options.count;
+    if (count === 0) {
+      await navigator.clearAppBadge();
+    } else {
+      await navigator.setAppBadge(count);
+    }
+    const value = count.toString();
+    localStorage.setItem(BadgeWeb.STORAGE_KEY, value);
+  }
+
+  public async clear(): Promise<void> {
+    await this.set({ count: 0 });
+  }
+
+  private async restore(): Promise<void> {
+    const value = localStorage.getItem(BadgeWeb.STORAGE_KEY);
+    if (!value) {
+      return;
+    }
+    const count = parseInt(value, 10);
+    await navigator.setAppBadge(count);
+  }
+}
+
+declare global {
+  interface Navigator {
+    setAppBadge: (count: number) => Promise<void>;
+    clearAppBadge: () => Promise<void>;
   }
 }

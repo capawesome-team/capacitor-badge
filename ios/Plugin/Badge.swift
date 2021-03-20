6bc1acdec1200070ac60cbafaf2561fcaa07840a
Robin Genz <mail@robingenz.dev>
2021-03-20 17:18:37 +0100

    feat: add `ios` support (#2)

--- ios/Plugin/Badge.swift
+++ ios/Plugin/Badge.swift
@@ -1,7 +1,38 @@
 import Foundation
+import Capacitor
 
 @objc public class Badge: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        return value
+    private let STORAGE_KEY = "capacitor.badge"
+    private var defaults: UserDefaults {
+        return UserDefaults.standard
+    }
+    
+    override init() {
+        super.init()
+        restore()
+    }
+    
+    @objc public func get() -> Int {
+        return defaults.integer(forKey: STORAGE_KEY)
+    }
+    
+    @objc public func set(count: Int, completion: @escaping () -> Void) {
+        DispatchQueue.main.async { [weak self] in
+            guard let strongSelf = self else {
+                return
+            }
+            UIApplication.shared.applicationIconBadgeNumber = count
+            strongSelf.defaults.set(count, forKey: strongSelf.STORAGE_KEY)
+            completion()
+        }
+    }
+    
+    @objc public func clear(completion: @escaping () -> Void) {
+        set(count: 0, completion: completion)
+    }
+    
+    @objc private func restore() {
+        let count = get()
+        set(count: count, completion: {})
     }
 }

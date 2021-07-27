c8471ffb0f5714ddb08689fefd01a61813ab91f7
Robin Genz <mail@robingenz.dev>
2021-07-27 14:45:01 +0200

    wip: ios

--- ios/Plugin/BadgePlugin.m
+++ ios/Plugin/BadgePlugin.m
@@ -8,5 +8,7 @@ CAP_PLUGIN(BadgePlugin, "Badge",
            CAP_PLUGIN_METHOD(checkPermissions, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(get, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(set, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(increase, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(decrease, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(clear, CAPPluginReturnPromise);
 )
